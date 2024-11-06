46af15b84c64a56e6f216da4e98b61e08f7fc54c
eleanora17 <eleanora17d@gmail.com>
2024-11-07 03:15:01 +0530

    build ready

--- frontend/src/pages/Add.tsx
+++ frontend/src/pages/Add.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "../../helpers/axiosConfig"; // Import your axios configuration
 import { useNavigate } from "react-router-dom"; // Import useNavigate for redirection
 
